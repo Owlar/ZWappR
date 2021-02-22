fa294529443091a714533b25a50c9207442fab14
Owlar <oscarlr@hiof.no>
2021-02-22 18:29:13 +0100

    Minor cleanup

--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:zwappr/features/activity/ui/activity_page.dart';
 import 'package:zwappr/features/chat/ui/chat_page.dart';
 import 'package:zwappr/features/feed/ui/feed_page.dart';

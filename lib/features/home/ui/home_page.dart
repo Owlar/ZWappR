79afe10a91b747c77c75387a5fdc6ad09fbb694a
Owlar <oscarlr@hiof.no>
2021-02-22 14:38:42 +0100

    Setup home page, add bottom navigation bar

--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:zwappr/features/authentication/services/authentication_service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  PageController _pageController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +28,41 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black54,
+          selectedItemColor: Theme.of(context).primaryColor,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Hjem",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: "Aktivitet",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: "Kart",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: "Chat",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profil",
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+      ),
     );
   }
-  
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+    _pageController.jumpToPage(index);
+  }
+
 }
+

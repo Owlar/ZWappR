4aa516c641940824810a64300b3ec4705eba054c
Owlar <oscarlr@hiof.no>
2021-02-22 18:26:49 +0100

    Add navigation by mouse click and horizontal scroll scrolling

--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:zwappr/features/authentication/services/authentication_service.dart';
+import 'package:zwappr/features/activity/ui/activity_page.dart';
+import 'package:zwappr/features/chat/ui/chat_page.dart';
+import 'package:zwappr/features/feed/ui/feed_page.dart';
+import 'package:zwappr/features/map/ui/map_page.dart';
+import 'package:zwappr/features/profile/ui/profile_page.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -12,25 +18,47 @@ class _HomePageState extends State<HomePage> {
   PageController _pageController;
 
   @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hjem"),
-            RaisedButton(
-              onPressed: () {
-                context.read<AuthenticationService>().signOut();
-              },
-              child: Text("Logg ut"),
-            ),
-          ],
-        ),
+      body: PageView.builder(
+        itemCount: 5,
+        controller: _pageController,
+        onPageChanged: (index) => setState(() => _selectedIndex = index),
+        itemBuilder: (BuildContext context, int index) {
+          switch(index) {
+            case 0:
+              return FeedPage();
+            case 1:
+              return ActivityPage();
+            case 2:
+              return MapPage();
+            case 3:
+              return ChatPage();
+            case 4:
+              return ProfilePage();
+            default:
+              throw "No page with index: $index";
+          }
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
           unselectedItemColor: Colors.black54,
           selectedItemColor: Theme.of(context).primaryColor,
+          type: BottomNavigationBarType.fixed,
+          iconSize: 30,
+          currentIndex: _selectedIndex,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
@@ -53,7 +81,6 @@ class _HomePageState extends State<HomePage> {
               label: "Profil",
             ),
           ],
-          currentIndex: _selectedIndex,
           onTap: _onItemTapped,
       ),
     );
@@ -62,6 +89,7 @@ class _HomePageState extends State<HomePage> {
   void _onItemTapped(int index) {
     setState(() => _selectedIndex = index);
     _pageController.jumpToPage(index);
+    print(index);
   }
 
 }

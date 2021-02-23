afb810e1640850742375b9f7520ec73fb609198f
bram <emilio.n.zimberlin@hiof.no>
2021-02-23 15:36:00 +0100

    added Camera function

--- lib/features/profile/ui/profile_page.dart
+++ lib/features/profile/ui/profile_page.dart
@@ -1,23 +1,90 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:zwappr/features/profile/ui/profliePicture.dart';
 
 class ProfilePage extends StatefulWidget {
+
   @override
   _ProfilePageState createState() => _ProfilePageState();
+
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  File _image;
+  final imagePicker = ImagePicker();
+
+  Future getImage() async{
+    final image = await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(image.path);
+    });
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Profile"),
-        backgroundColor: Color(0xFF1E1E),
-      ),
+      appBar: AppBar(),
         body:  Center(
-          child: ProfilePicture(),
-          
+          child: Column(
+            children: [
+              SizedBox(
+                height: 115,
+                width: 115,
+                child: Stack(
+                  fit: StackFit.expand,
+                  overflow: Overflow.visible,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: _image == null ? AssetImage("assets/images/profile_test.png") : FileImage(_image),
+                    ),
+                    Positioned(
+                      right: -12,
+                      bottom: 0,
+                      child: SizedBox(
+                          height: 46,
+                          width: 46,
+                          child: FlatButton(
+
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                                side: BorderSide(color: Color(0xFFFFFF))),
+                            color: Color(0xFFC9C9C9),
+                            onPressed: getImage,
+                            child: SvgPicture.asset("assets/icons/photo_camera-24px.svg"),
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ), SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: FlatButton(
+                    padding: EdgeInsets.all(20),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                    color: Color(0xFFBFBEBE),
+                    onPressed: (){},
+                    child: Row(
+                      children: [Expanded(child: Text("UserName"))],
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: FlatButton(
+                    padding: EdgeInsets.all(20),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                    color: Color(0xFFBFBEBE),
+                    onPressed: (){},
+                    child: Row(
+                      children: [Expanded(child: Text("Email"))],
+                    )),
+              )
+            ],
+          ),
         )
     );
   }
--- lib/features/profile/ui/profliePicture.dart
+++ lib/features/profile/ui/profliePicture.dart
@@ -1,11 +1,27 @@
 
 
+
+
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
+import 'profile_page.dart';
+
+// ignore: must_be_immutable
 class ProfilePicture extends StatelessWidget{
+  File _image;
+  final imagePicker = ImagePicker();
 
+  Future getImage() async{
+    final image = await imagePicker.getImage(source: ImageSource.camera);
+
+     _image = File(image.path);
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +35,7 @@ class ProfilePicture extends StatelessWidget{
             overflow: Overflow.visible,
             children: [
               CircleAvatar(
-                backgroundImage: AssetImage("assets/images/profile_test.png"),
+                backgroundImage: _image == null ? AssetImage("assets/images/profile_test.png") : FileImage(_image),
               ),
               Positioned(
                 right: -12,
@@ -28,19 +44,35 @@ class ProfilePicture extends StatelessWidget{
                   height: 46,
                   width: 46,
                   child: FlatButton(
+
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(50),
                         side: BorderSide(color: Color(0xFFFFFF))),
-                    color: Color(0xFFF5f6F9),
-                    onPressed: () {},
+                    color: Color(0xFFC9C9C9),
+                    onPressed: getImage,
                     child: SvgPicture.asset("assets/icons/photo_camera-24px.svg"),
                   )
                 ),
               )
             ],
           ),
+        ), SizedBox(height: 20,),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: FlatButton(
+            padding: EdgeInsets.all(20),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              color: Color(0xFFBFBEBE),
+              onPressed: (){},
+              child: Row(
+                children: [],
+              )),
         )
       ],
     );
   }
+
+
+
+
 }

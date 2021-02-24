9c424b388bf1d53b2a99ccbf15a2a20302cd37c2
bram <emilio.n.zimberlin@hiof.no>
2021-02-24 14:05:09 +0100

    Added buttons, settings and edit and background

--- lib/features/profile/ui/profile_page.dart
+++ lib/features/profile/ui/profile_page.dart
@@ -23,63 +23,99 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-        body:  Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 115,
-                width: 115,
-                child: Stack(
-                  fit: StackFit.expand,
-                  overflow: Overflow.visible,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: _image == null ? AssetImage("assets/images/profile_test.png") : FileImage(_image),
-                    ),
-                    Positioned(
-                      right: -12,
-                      bottom: 0,
-                      child: SizedBox(
-                          height: 46,
-                          width: 46,
-                          child: FlatButton(
+        body:  Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background_screen.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  child: SizedBox(
+                    height: 115,
+                    width: 115,
+                    child: Stack(
+                      fit: StackFit.expand,
+                      overflow: Overflow.visible,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: _image == null ? AssetImage("assets/images/profile_test.png") : FileImage(_image),
+                        ),
+                        Positioned(
+                          right: -12,
+                          bottom: 0,
+                          child: SizedBox(
+                              height: 46,
+                              width: 46,
+                              child: FlatButton(
 
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                                side: BorderSide(color: Color(0xFFFFFF))),
-                            color: Color(0xFFC9C9C9),
-                            onPressed: getImage,
-                            child: SvgPicture.asset("assets/icons/photo_camera-24px.svg"),
-                          )
-                      ),
-                    )
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50),
+                                    side: BorderSide(color: Color(0xFFFFFF))),
+                                color: Color(0xFFE0E0E0),
+                                onPressed: getImage,
+                                child: SvgPicture.asset("assets/icons/photo_camera-24px.svg"),
+                              )
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ), SizedBox(height: 20,),
+                Text("Tina Olsen"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal:20, vertical: 0),
+                          child: SizedBox(
+                            height: 46,
+                            width: 46,
+                            child: IconButton(
+                              icon: Icon(Icons.settings),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:20, vertical: 0),
+                      child: SizedBox(
+                        height: 46,
+                        width: 46,
+                          child: IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {},
+                          ),
+                        ),
+                    ),
                   ],
                 ),
-              ), SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: FlatButton(
-                    padding: EdgeInsets.all(20),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                    color: Color(0xFFBFBEBE),
-                    onPressed: (){},
-                    child: Row(
-                      children: [Expanded(child: Text("UserName"))],
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: FlatButton(
-                    padding: EdgeInsets.all(20),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                    color: Color(0xFFBFBEBE),
-                    onPressed: (){},
-                    child: Row(
-                      children: [Expanded(child: Text("Email"))],
-                    )),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 125, vertical: 0),
+                  child: ButtonTheme(
+
+                    height: 20,
+                    minWidth: 100,
+                    child: FlatButton(
+
+                        padding: EdgeInsets.all(20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            side: BorderSide(color: Color(0xFFFFFF))),
+                        color: Color(0xFFE0E0E0),
+                        onPressed: (){},
+                        child: Row(
+                          children: [Expanded(child: Text("Inviter",
+                            textAlign: TextAlign.center,))],
+                        )),
+                  ),
+                )
+              ],
+            ),
           ),
         )
     );

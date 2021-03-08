0100edd41306dc076db02a3e78bcf3a1d3db970f
Owlar <oscarlr@hiof.no>
2021-03-08 14:05:20 +0100

    Add listview, add floating action button

---
+++ lib/features/things/ui/new_thing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class NewThingPage extends StatefulWidget {
+  @override
+  _NewThingPageState createState() => _NewThingPageState();
+}
+
+class _NewThingPageState extends State<NewThingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Ny ting"),
+    );
+  }
+}
--- lib/features/things/ui/things_page.dart
+++ lib/features/things/ui/things_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'new_thing_page.dart';
 
 class ThingsPage extends StatefulWidget {
   @override
@@ -8,7 +11,33 @@ class ThingsPage extends StatefulWidget {
 class _ThingsPageState extends State<ThingsPage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Observer(
+        builder: (_) => ListView.builder(
+          padding: const EdgeInsets.all(14.0),
+          // TODO: Set to length of items list
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Observer(
+              // TODO: Create card widget
+              /*builder: (_) => ThingItem(
 
+              )*/
+            );
+          }
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => NewThingPage())
+        ),
+        label: Text("Ny ting"),
+        icon: Icon(Icons.add),
+      )
+
+    );
   }
 
 }

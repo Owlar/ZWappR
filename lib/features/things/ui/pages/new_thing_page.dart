e00295d2924e3856d6a7d18bcf438698d3f1e03c
Owlar <oscarlr@hiof.no>
2021-03-15 03:08:38 +0100

    Add form validation

--- lib/features/things/ui/pages/new_thing_page.dart
+++ lib/features/things/ui/pages/new_thing_page.dart
@@ -14,6 +14,8 @@ class _NewThingPageState extends State<NewThingPage> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,38 +30,55 @@ class _NewThingPageState extends State<NewThingPage> {
           child: Column(
             children: <Widget> [
               SizedBox(height: 42),
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  labelText: "Tittel",
-                ),
-              ),
-              TextField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  labelText: "Beskrivelse",
-                ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "Tittel"
+                      ),
+                      controller: titleController,
+                      validator: (value) {
+                        if (value.isEmpty) return "Vennligst skriv inn tittel";
+                        else return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Beskrivelse"
+                      ),
+                      controller: descriptionController,
+                      validator: (value) {
+                        if (value.isEmpty) return "Vennligst skriv inn beskrivelse";
+                        else return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    RaisedButton(
+                      color: zwapprBlack,
+                      textColor: zwapprWhite,
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          final newThing = ThingModel(
+                              title: titleController.text.trim(),
+                              description: descriptionController.text.trim(),
+                              imageUrl: "https://images.unsplash.com/photo-1488109811119-98431feb6929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+                          );
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text("Lag Gjenstand"),
+                    ),
+                  ],
+                )
               ),
               SizedBox(height: 42),
               FlatButton(
-                onPressed: () {
-                  // TODO: Let user take picture with camera or choose existing picture from gallery (Like in profile page)
-                },
-                child: Image.network("https://images.unsplash.com/photo-1488109811119-98431feb6929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80")
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                color: zwapprBlack,
-                textColor: zwapprWhite,
-                onPressed: () async {
-                  final newThing = ThingModel(
-                      title: titleController.text.trim(),
-                      description: descriptionController.text.trim(),
-                      imageUrl: "https://images.unsplash.com/photo-1488109811119-98431feb6929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                  );
-                  Navigator.pop(context);
-                },
-                child: Text("Lag Gjenstand"),
+                  onPressed: () {
+                    // TODO: Let user take picture with camera or choose existing picture from gallery (Like in profile page)
+                  },
+                  child: Image.network("https://images.unsplash.com/photo-1488109811119-98431feb6929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80")
               ),
             ],
           )

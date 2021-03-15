468de22406636841c0c7c3f63a23c99974d00acf
Owlar <oscarlr@hiof.no>
2021-03-15 01:53:22 +0100

    Remove and comment out dev testing

--- lib/features/things/ui/pages/things_page.dart
+++ lib/features/things/ui/pages/things_page.dart
@@ -48,7 +48,8 @@ class _ThingsPageState extends State<ThingsPage> {
               context,
               MaterialPageRoute(builder: (context) => NewThingPage())
           );
-          // Test objects
+          // Creating objects for testing purposes
+          /*
           final ThingModel watch = ThingModel(
               title: "Klokke Rolex",
               description: "Pent brukt Rolex Submariner",
@@ -61,17 +62,10 @@ class _ThingsPageState extends State<ThingsPage> {
               numberOfLikes: 19,
               imageUrl: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80"
           );
-
-          // 1. Creating
           _thingsService.create(watch);
           _thingsService.create(clothes);
-
-          // 2. Listing
-          List<ThingModel> things = List();
-          things = await _thingsService.getAll();
-          print("Should print all things below:");
-          print(things);
-
+          */
+          
 
         },
         label: Text("Ny ting"),

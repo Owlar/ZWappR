13baa4cf1583b097956ad48811af5ffe4bc78d13
bram <emilio.n.zimberlin@hiof.no>
2021-03-15 12:44:24 +0100

    added photoPicker

--- lib/features/things/ui/pages/new_thing_page.dart
+++ lib/features/things/ui/pages/new_thing_page.dart
@@ -1,6 +1,7 @@
-
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:zwappr/features/things/models/thing_model.dart';
 import 'package:zwappr/features/things/services/i_things_service.dart';
 import 'package:zwappr/features/things/services/things_service.dart';
@@ -18,6 +19,69 @@ class _NewThingPageState extends State<NewThingPage> {
   final TextEditingController descriptionController = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
+  File _image;
+  final imagePicker = ImagePicker();
+  List<String> imageList;
+
+
+  Future getImage() async {
+    final image = await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(image.path);
+    });
+   //uploadPic(_image);
+  }
+
+  Future getGallery() async {
+    final image = await imagePicker.getImage(
+        source: ImageSource.gallery, imageQuality: 50
+    );
+    setState(() {
+      _image = File(image.path);
+    });
+    //uploadPic(_image);
+  }
+
+  Future<void> photoPicker() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title:  Text("Camera or Gallery"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text("Choose camera or gallery"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: new Icon(
+                Icons.camera_alt,
+                color: zwapprGreen,
+              ),
+              onPressed: () {
+                getImage();
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: new Icon(
+                Icons.insert_photo,
+                color: zwapprGreen,
+              ),
+              onPressed: () {
+                getGallery();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,6 +144,8 @@ class _NewThingPageState extends State<NewThingPage> {
               SizedBox(height: 42),
               FlatButton(
                   onPressed: () {
+                    print('LOL');
+                    photoPicker();
                     // TODO: Let user take picture with camera or choose existing picture from gallery (Like in profile page)
                   },
                   child: Image.network("https://images.unsplash.com/photo-1488109811119-98431feb6929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80")

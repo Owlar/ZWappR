dc19589276089944183f8cf0eba0d8a45659d43b
Owlar <oscarlr@hiof.no>
2021-03-18 12:34:32 +0100

    Remove commented out code

--- lib/features/things/ui/pages/things_page.dart
+++ lib/features/things/ui/pages/things_page.dart
@@ -46,28 +46,10 @@ class _ThingsPageState extends State<ThingsPage> {
               context,
               MaterialPageRoute(builder: (context) => NewThingPage())
           );
-          // Creating objects for testing purposes
-          /*
-          final ThingModel watch = ThingModel(
-              title: "Klokke Rolex",
-              description: "Pent brukt Rolex Submariner",
-              numberOfLikes: 8,
-              imageUrl: "https://images.unsplash.com/photo-1611243705491-71487c2ed137?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-          );
-          final ThingModel clothes = ThingModel(
-              title: "Diverse klær",
-              description: "Diverse klær selges pga. oppgradering av garderoben. Kom med bud!",
-              numberOfLikes: 19,
-              imageUrl: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80"
-          );
-          _thingsService.create(watch);
-          _thingsService.create(clothes);
-          */
         },
         label: Text("Ny ting"),
         icon: Icon(Icons.add),
       )
-
     );
   }
 

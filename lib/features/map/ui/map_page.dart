6c424c7e5ce6db222c3788164b8ca8f00910c571
Owlar <oscarlr@hiof.no>
2021-03-20 17:21:11 +0100

    Add get user location

--- lib/features/map/ui/map_page.dart
+++ lib/features/map/ui/map_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -11,16 +12,26 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   Completer<GoogleMapController> _controller = Completer();
 
+  String _currentPositionLatitude;
+  String _currentPositionLongitude;
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
+  /*static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+      zoom: 19.151926040649414
+  );*/
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,20 +41,37 @@ class _MapPageState extends State<MapPage> {
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
+          zoomControlsEnabled: false,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
         ),
         floatingActionButton: FloatingActionButton.extended(
-            onPressed: _goToTheLake,
-            label: Text("To the Lake!"),
-            icon: Icon(Icons.directions_boat)
+            onPressed: _filter,
+            icon: Icon(Icons.filter_list),
+            label: Text("Filter")
         )
+
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Future<void> _filter() async {
+    //final GoogleMapController controller = await _controller.future;
+    //controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    print(_currentPositionLatitude);
+    print(_currentPositionLongitude);
   }
 
-  // Source:
-  // https://pub.dev/packages/google_maps_flutter
+  void _getCurrentPosition() async {
+    final geoPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    setState(() {
+      _currentPositionLatitude = "${geoPosition.latitude}";
+      _currentPositionLongitude = "${geoPosition.longitude}";
+    });
+  }
+
+  void _goToMyLocation() {
+
+  }
+
+
 }

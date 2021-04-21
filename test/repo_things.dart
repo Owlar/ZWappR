338506c9388de49e016f8606c5f091aa31aa49b5
bram <emilio.n.zimberlin@hiof.no>
2021-04-21 23:32:55 +0200

    added some tests(not working)

---
+++ test/repo_things.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:zwappr/features/things/models/thing_model.dart';
+
+import 'repo_things.mocks.dart';
+
+
+final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+// Generate a MockClient using the Mockito package.
+// Create new instances of this class in each test.
+@GenerateMocks([http.Client])
+void main() {
+
+  group('fetchAlbum', () {
+    test('returns an Data if the http call completes successfully', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client.get("https://us-central1-zwappr.cloudfunctions.net/api/things/me"))
+          .thenAnswer((_) async => http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200));
+
+      expect(await getAll(client), isA<List<ThingModel>>());
+    });
+
+   /* test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(client.get("https://us-central1-zwappr.cloudfunctions.net/api/things/me"))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(getAll(client), throwsException);
+    });*/
+  });
+}
+Future<List<ThingModel>> getAll(http.Client client) async {
+  final response = await client.get(
+      "https://us-central1-zwappr.cloudfunctions.net/api/things/me",
+      headers: <String, String>{
+        "Content-Type": "application/json; charset=UTF-8",
+        "idToken": await _firebaseAuth.currentUser.getIdToken()
+      }
+  );
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> parsed = jsonDecode(response.body);
+    return List<ThingModel>.from(parsed["data"].map((x) => ThingModel.fromJson(x)));
+  } else {
+    print("Statuscode is " + response.statusCode.toString());
+    throw Exception("Failed to fetch data");
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,2 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zwappr/features/activity/models/chat_message.dart';
+import 'package:zwappr/features/activity/ui/pages/favorite_page.dart';
+import 'package:zwappr/features/activity/ui/widgets/chat_info_person.dart';
+import 'package:zwappr/features/activity/ui/widgets/list_view_msg.dart';
+import 'package:zwappr/features/profile/ui/widgets/back_btn_blue.dart';
+import 'package:zwappr/features/things/models/thing_model.dart';
+
 void main() {
+  // Define a test. The TestWidgets function also provides a WidgetTester
+  // to work with. The WidgetTester allows building and interacting
+  // with widgets in the test environment.
+
+  test('Counter value should be incremented', () {
+    final thing = ThingModel(title: "title", description: "description", imageUrl: "imageUrl", exchangeValue: "exchangeValue", condition: "condition", category: "category", latitude: 1, longitude: 2);
+    expect(thing.title, "title");
+    expect(thing.description, "description");
+    expect(thing.imageUrl, "imageUrl");
+    expect(thing.exchangeValue, "exchangeValue");
+    expect(thing.condition, "condition");
+    expect(thing.category, "category");
+    expect(thing.latitude, 1);
+    expect(thing.longitude, 2);
+
+  });
+
+ /* testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(FavoritePage());
+  });
+  testWidgets('ChatMessage has a List of messages', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    ChatMessage message1 = new ChatMessage(messageContent: "messageContent", messageType: "messageType");
+    await tester.pumpWidget(ListViewMsg(messages: [message1],));
+
+
+  });
+
+    testWidgets('ChatInfoPerson has a Name and Image', (WidgetTester tester) async {
+      // Create the widget by telling the tester to build it.
+
+      await tester.pumpWidget(ChatInfoPerson(name: "name", image: "image"));
+
+    });
+
+  testWidgets('BackBtnBlue ', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+
+    await tester.pumpWidget(BackBtnBlue());
+
+  });
+  testWidgets('BackBtnBlue ', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+
+    await tester.pumpWidget(IconButton(icon: Icon(Icons.insert_photo), onPressed: (){}));
+
+  });
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
+  });*/
+}
+class MyWidget extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const MyWidget({
+    Key key,
+    @required this.title,
+    @required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
 }
